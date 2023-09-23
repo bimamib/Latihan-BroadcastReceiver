f1b3ecfa0990ebed8e8f451a2e9d762638a9da31
bimamib <bimaprast1601@gmail.com>
2023-09-24 01:57:57 +0700

    menambahkan kode untuk meminta permission ke dalam MainActivity

--- app/src/main/java/com/bima/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/bima/mybroadcastreceiver/MainActivity.kt
@@ -3,6 +3,9 @@ package com.bima.mybroadcastreceiver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import android.Manifest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bima.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -17,9 +20,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding?.btnPermission?.setOnClickListener(this)
     }
 
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onClick(v: View) {
         when (v.id) {
-
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
         }
     }
 
